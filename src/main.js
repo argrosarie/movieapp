ac0e438ff71cba569fba8c797885634a6b07debb
rosarie <rosariosanchezsampietro@gmail.com>
2023-02-20 10:30:55 -0300

    Axios implemented

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    api_key: API_KEY,
+  },
+})
+
 async function getTrendMoviesPreview() {
-  const res = await fetch(
-    'https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY,
-  )
-  const data = await res.json()
+  const { data } = await api('trending/movie/day')
 
   const movies = data.results
   movies.forEach((movie) => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    const trendingPreviewMoviesContainer = document.querySelector(
+      '#trendingPreview .trendingPreview-movieList',
+    )
 
     const movieContainer = document.createElement('div')
     movieContainer.classList.add('movie-container')
@@ -17,38 +26,34 @@ async function getTrendMoviesPreview() {
     movieImg.setAttribute(
       'src',
       'https://image.tmdb.org/t/p/w300/' + movie.poster_path,
-    );
+    )
     movieContainer.appendChild(movieImg)
     trendingPreviewMoviesContainer.appendChild(movieContainer)
   })
 }
 getTrendMoviesPreview()
 
-
 async function getCategoriesPreview() {
-    const res = await fetch(
-      'https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY,
+  const { data } = await api('genre/movie/list')
+
+  const categories = data.genres
+  categories.forEach((category) => {
+    const previewCategoriesContainer = document.querySelector(
+      '#categoriesPreview .categoriesPreview-list',
     )
-    const data = await res.json()
-  
-    const categories = data.genres
-    categories.forEach((category) => {
-      const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
-  
-      const categoryContainer = document.createElement('div')
-      categoryContainer.classList.add('category-container')
-  
-      const categoryTitle = document.createElement('h3')
-      categoryTitle.classList.add('category-title')
-      categoryTitle.setAttribute('id', 'id' + category.id)
-      const categoryTitleText = document.createTextNode(category.name)
 
-      categoryTitle.appendChild(categoryTitleText)
-      categoryContainer.appendChild(categoryTitle)
+    const categoryContainer = document.createElement('div')
+    categoryContainer.classList.add('category-container')
 
-      previewCategoriesContainer.appendChild(categoryContainer)
-    
-    })
-  }
-  getCategoriesPreview()
-  
+    const categoryTitle = document.createElement('h3')
+    categoryTitle.classList.add('category-title')
+    categoryTitle.setAttribute('id', 'id' + category.id)
+    const categoryTitleText = document.createTextNode(category.name)
+
+    categoryTitle.appendChild(categoryTitleText)
+    categoryContainer.appendChild(categoryTitle)
+
+    previewCategoriesContainer.appendChild(categoryContainer)
+  })
+}
+getCategoriesPreview()
