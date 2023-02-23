1af95be4544f802fb04ed7318007675d85af1a77
rosarie <rosariosanchezsampietro@gmail.com>
2023-02-23 08:31:12 -0300

    Refactor getCategoriesPrevieww

--- src/main.js
+++ src/main.js
@@ -26,19 +26,9 @@ function createMovies(movies, container) {
   })
 }
 
-async function getTrendMoviesPreview() {
-  const { data } = await api('trending/movie/day')
-  const movies = data.results
-  createMovies(movies, trendingMoviesPreviewList)
- 
-}
+function createCategories(categories, container) {
+  container.innerHTML = ''
 
-async function getCategoriesPreview() {
-  const { data } = await api('genre/movie/list')
-
-  categoriesPreviewList.innerHTML = ''
-
-  const categories = data.genres
   categories.forEach((category) => {
     const categoryContainer = document.createElement('div')
     categoryContainer.classList.add('category-container')
@@ -54,8 +44,21 @@ async function getCategoriesPreview() {
     categoryTitle.appendChild(categoryTitleText)
     categoryContainer.appendChild(categoryTitle)
 
-    categoriesPreviewList.appendChild(categoryContainer)
+    container.appendChild(categoryContainer)
   })
+}
+
+async function getTrendMoviesPreview() {
+  const { data } = await api('trending/movie/day')
+  const movies = data.results
+  createMovies(movies, trendingMoviesPreviewList)
+}
+
+async function getCategoriesPreview() {
+  const { data } = await api('genre/movie/list')
+
+  const categories = data.genres
+  createCategories(categories, categoriesPreviewList)
 }
 
 async function getMoviesByCategory(id) {
@@ -65,23 +68,7 @@ async function getMoviesByCategory(id) {
     },
   })
 
-  trendingMoviesPreviewList.innerHTML = ''
-
   const movies = data.results
-  genericSection.innerHTML = ''
-
-  movies.forEach((movie) => {
-    const movieContainer = document.createElement('div')
-    movieContainer.classList.add('movie-container')
-
-    const movieImg = document.createElement('img')
-    movieImg.classList.add('movie-img')
-    movieImg.setAttribute('alt', movie.title)
-    movieImg.setAttribute(
-      'src',
-      'https://image.tmdb.org/t/p/w300/' + movie.poster_path,
-    )
-    movieContainer.appendChild(movieImg)
-    genericSection.appendChild(movieContainer)
-  })
+  createMovies(movies, genericSection)
+  
 }
