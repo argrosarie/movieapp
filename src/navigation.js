8b51f1b995a8f3cf8e3a52405d8f98f3e1a30cc5
rosarie <rosariosanchezsampietro@gmail.com>
2023-02-23 10:10:50 -0300

    Search movie

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,5 @@
 searchFormBtn.addEventListener('click', () => {
-  location.hash = "#search="
+  location.hash = "#search=" + searchFormInput.value
 })
 trendingBtn.addEventListener('click', () => {
   location.hash = "#trends"
@@ -43,18 +43,20 @@ function trendsPage() {
   movieDetailSection.classList.remove('inactive')
 }
 function searchPage() {
-  headerSection.classList.remove('header-container--long')
-  headerSection.style.background = ''
-  arrowBtn.classList.remove('inactive')
-  arrowBtn.classList.remove('header-arrow--white')
-  headerTitle.classList.add('inactive')
-  headerCategoryTitle.classList.remove('inactive')
-  searchForm.classList.remove('inactive')
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.remove('inactive');
 
-  trendingPreviewSection.classList.add('inactive')
-  categoriesPreviewSection.classList.add('inactive')
-  genericSection.classList.add('inactive')
-  movieDetailSection.classList.remove('inactive')
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
+  const [_, query] = location.hash.split('=');
+  getMoviesBySearch(query);
 }
 function movieDetailsPage() {
   headerSection.classList.add('header-container--long')
